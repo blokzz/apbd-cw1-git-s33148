48185325445b04f4687ecffa4f2a96840d98e324
Kamil Bobrzak <s33148@pjwstk.edu.pl>
2026-03-11 09:45:41 +0100

    Stworzono klase Student

---
+++ Student.cs
@@ -0,0 +1,16 @@
+﻿namespace apbdcwiczenie;
+
+public class Student
+{
+    private string name;
+    private int age;
+
+    public Student(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    
+    public string getName()=>this.name;
+    public int getAge()=>this.age;
+}
