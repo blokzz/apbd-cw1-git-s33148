9d9ed1442c6e7668294f2806698202c473d16bee
Kamil Bobrzak <s33148@pjwstk.edu.pl>
2026-03-11 09:43:36 +0100

    dodano Hashset i wypelniono go dwoma liczbami

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        HashSet<int> set = new HashSet<int>();
+        set.Add(1);
+        set.Add(2);
     }
 }
